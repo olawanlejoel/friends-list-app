bfd1828cee336700d0ad34f245d7d99b9cb2c192
Olawanle Joel <info4tobest2@gmail.com>
2021-05-08 06:23:08 +0100

    configuration push

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// Inside vue.config.js
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: "My App",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  },
+};
