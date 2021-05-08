50c5a1d543628fcbc64f4a22bbcb53fa62a2efd1
Olawanle Joel <info4tobest2@gmail.com>
2021-05-08 07:44:47 +0100

    fix caching

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,13 @@ module.exports = {
     msTileColor: "#000000",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "src/service-worker.js",
+      // ...other Workbox options...
+    },
   },
 };
